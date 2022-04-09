eb0cd8633b18021a21ad90e22c4a33e83eb6b577
David Albrecht <davidpabloalbrecht@gmail.com>
2022-04-09 11:22:56 -0700

    add docstrings

--- model/tag_extractor.py
+++ model/tag_extractor.py
@@ -14,8 +14,24 @@ logging.basicConfig(
 from .text_cleaner import TextCleaner
 
 
-# TODO: Add tests and docstrings
+# TODO: Add tests
 class TagExtractor:
+    """
+    The TagExtractor trains an LSI model and uses it to extract tags and rank candidates.
+    
+    Args:
+        word_count_min (int): The minimum number of times a word has to appear to be kept. Optional, default is 2.
+        word_length_min (int): The minimum length a word must be to be kept. Optional, default is 2.
+        num_lsi_topics (int): The number of LSI topics to extract during model training. 
+            Optional, default is 300. Optimal values are known to be 100-400.
+        bigram_kwargs (dict): Bigram arguments. 
+            bigrams_pmi_min_value: The minimum value of Pointwise Mutual Information for a bigram to be kept. 
+                Values above 1 are best. Optional, default is 1.
+            bigrams_min_freq: The minimum number of times a bigram has to appear to be kept. 
+                Optional, default is 20.
+            Optional, default is {'bigrams_pmi_min_value':1,'bigrams_min_freq':20}.
+    """
+    
     def __init__(self, 
                  word_count_min=2, 
                  word_length_min=2, 
@@ -27,10 +43,34 @@ class TagExtractor:
         self.bigram_kwargs = bigram_kwargs
         
     def save(self, fname=None):
+        """
+        Saves current model artifacts in the pickle format.
+        
+        Args:
+            fname (str): File name to save to. Optional, default 'TagExtractor.p'.
+            
+        Returns:
+            None.
+        
+        Raises:
+            None.
+        """
         with open('TagExtractor.p' if fname is None else fname, 'wb') as f:
             pickle.dump(self.__dict__, f)
     
     def load(self, fname=None):
+        """
+        Loads model artifacts from the pickle format.
+        
+        Args:
+            fname (str): File name to load. Optional, default 'TagExtractor.p'.
+            
+        Returns:
+            None.
+        
+        Raises:
+            None.
+        """
         self.__dict__.update(
             pickle.loads(
                 open('TagExtractor.p' if fname is None else fname, 'rb').read()
@@ -38,6 +78,18 @@ class TagExtractor:
         )
     
     def fit(self, documents):
+        """
+        Fits model to a set of documents.
+        
+        Args:
+            documents (list of strings): Documents to model.
+            
+        Returns:
+            None.
+            
+        Raises:
+            None.
+        """
         # Clean text
         self.tc = TextCleaner(word_count_min=self.word_count_min, 
                               word_length_min=self.word_length_min,
@@ -77,6 +129,19 @@ class TagExtractor:
         logger.info('Tag Extractor training is done!')
         
     def _get_vector_representations(self, document):
+        """
+        Helper method to convert a document into its TF-IDF and LSI vector counterparts.
+        
+        Args:
+            document (str): Document to convert to vectors.
+            
+        Raises:
+            None.
+            
+        Returns:
+            corpus_tfidf (list of tuples): Tuples containing word IDs and TF-IDF values.
+            corpus_lsi (list of tuples): Tuples containing word IDs and LSI values.
+        """
         doc_ind = self.doc2ind.get(document) 
         if doc_ind is None:
             cleaned_documents = self.tc.transform([document])
@@ -88,59 +153,79 @@ class TagExtractor:
             corpus_lsi = self.corpus_lsi[doc_ind]
         return corpus_tfidf, corpus_lsi
     
-    def transform(self, ranked_inputs, 
-                  n_input_tags=10, n_candidate_tags=5, 
-                  selected_tags=[]):
+    def transform(self, input_document, candidate_documents,
+                  n_input_tags=10, n_candidate_tags=5):
+        """
+        Transforms pairs of input and candidate documents into their respective tag representations.
         
-        if isinstance(ranked_inputs[0], dict):
-            if len(selected_tags) > 0:
-                reranking_scores = []
-                for tag_dict in ranked_inputs:
-                    document_score = sum(tag_dict.get(tag,0) for tag in selected_tags)
-                    reranking_scores.append(document_score)
-                ranking = np.argsort(-np.array(reranking_scores)).tolist()
-            else:
-                ranking = list(range(len(ranked_inputs)))
-
-            return ranking
-        
-        if isinstance(ranked_inputs[0], str):
-            input_document = ranked_inputs[0]
-            candidate_documents = ranked_inputs[1:]
-
-            tfidf_input, lsi_input = self._get_vector_representations(input_document)
-            if (len(tfidf_input) == 0) | (len(lsi_input) == 0):
-                return [], [], []
-
-            candidate_tags = []
-            for candidate_document in candidate_documents:
-                tfidf_candidate, lsi_candidate = self._get_vector_representations(candidate_document)
-
-                if (len(tfidf_candidate) == 0) | (len(lsi_candidate) == 0):
-                    candidate_tags.append({})
-                else:
-
-                    # Get shared words using the sparse TF-IDF corpus
-                    shared_word_ids = np.array(list(set(np.array(tfidf_input)[:,0]) \
-                                                  & set(np.array(tfidf_candidate)[:,0]))).astype(int)
-
-                    # Get word-topic vectors for those shared words
-                    shared_word_topics = self.lsi_topic_matrix.T[shared_word_ids]
-
-                    # Sum those word-topic vectors weighed by each document's topic loadings
-                    shared_lsi_word_loadings = np.sum((shared_word_topics*np.array(lsi_input)[:,1]) \
-                                                    + (shared_word_topics*np.array(lsi_candidate)[:,1]), axis=1)
-
-                    # Rank shared words by highest weighed word-topic loadings
-                    ranking = np.argsort(shared_lsi_word_loadings)[::-1]
-                    ranked_shared_word_ids = shared_word_ids[ranking]
-
-                    tags = [self.dictionary[r] for r in ranked_shared_word_ids][:n_candidate_tags]
-                    scores = shared_lsi_word_loadings[ranking][:n_candidate_tags].tolist()
-
-                    candidate_tags.append(dict(zip(tags, scores)))
-
-            all_candidate_tags = list(itertools.chain(*[list(ct.keys()) for ct in candidate_tags]))
-            input_tags = Counter(all_candidate_tags).most_common(n_input_tags)
+        Args:
+            input_document (str): Document of input item.
+            candidate_documents (list of strings): Documents of candidate items.
+            n_input_tags (int): Maximum number of most common tags to aggregate associated with the input item.
+            n_candidate_tags (int): Maximum of tags to extract from each candidate item.
             
-            return input_tags, candidate_tags
+        Raises:
+            None.
+            
+        Returns:
+            input_tags (ordered list of tuples): Tuples of tags and number of times extracted from candidate_documents.
+            candidate_tags (ordered list of tuples): Tuples of tags and LSI values.
+        """
+        tfidf_input, lsi_input = self._get_vector_representations(input_document)
+        if (len(tfidf_input) == 0) | (len(lsi_input) == 0):
+            return [], [], []
+
+        candidate_tags = []
+        for candidate_document in candidate_documents:
+            tfidf_candidate, lsi_candidate = self._get_vector_representations(candidate_document)
+
+            if (len(tfidf_candidate) == 0) | (len(lsi_candidate) == 0):
+                candidate_tags.append({})
+            else:
+
+                # Get shared words using the sparse TF-IDF corpus
+                shared_word_ids = np.array(list(set(np.array(tfidf_input)[:,0]) \
+                                              & set(np.array(tfidf_candidate)[:,0]))).astype(int)
+
+                # Get word-topic vectors for those shared words
+                shared_word_topics = self.lsi_topic_matrix.T[shared_word_ids]
+
+                # Sum those word-topic vectors weighed by each document's topic loadings
+                shared_lsi_word_loadings = np.sum((shared_word_topics*np.array(lsi_input)[:,1]) \
+                                                + (shared_word_topics*np.array(lsi_candidate)[:,1]), axis=1)
+
+                # Rank shared words by highest weighed word-topic loadings
+                ranking = np.argsort(shared_lsi_word_loadings)[::-1]
+                ranked_shared_word_ids = shared_word_ids[ranking]
+
+                tags = [self.dictionary[r] for r in ranked_shared_word_ids][:n_candidate_tags]
+                scores = shared_lsi_word_loadings[ranking][:n_candidate_tags].tolist()
+
+                candidate_tags.append(dict(zip(tags, scores)))
+
+        all_candidate_tags = list(itertools.chain(*[list(ct.keys()) for ct in candidate_tags]))
+        input_tags = Counter(all_candidate_tags).most_common(n_input_tags)
+
+        return input_tags, candidate_tags
+        
+    def rank(self, candidate_tags, selected_tags):
+        """
+        Ranks an ordered list of candidate tags given selected tags.
+        
+        Args:
+            candidate_tags (list of dictionaries): candidate_tags from the transform() method.
+                Ordered dictionaries containing candidate tags and associated LSI values.
+            selected_tags (list of strings): Tags chosen for re-ranking candidates.
+            
+        Raises:
+            None.
+            
+        Returns:
+            ranking (list of ints): List of indices in which to re-rank candidates.
+        """
+        reranking_scores = []
+        for tag_dict in candidate_tags:
+            document_score = sum(tag_dict.get(tag,0) for tag in selected_tags)
+            reranking_scores.append(document_score)
+        ranking = np.argsort(-np.array(reranking_scores)).tolist()
+        return ranking
--- model/text_cleaner.py
+++ model/text_cleaner.py
@@ -16,11 +16,26 @@ logging.basicConfig(
 
 
 def _tqdm(iterator, display, desc=None):
+    """
+    Helper wrapper over tqdm to control displaying progress.
+    
+    Args:
+        iterator (list, generator): Iterator to iterate through.
+        display (bool): Whether to display progress.
+        desc (str): Explanation for work on the iterator. Optional, default is None.
+        
+    Raises:
+        None.
+        
+    Returns:
+        iterator (list, generator, tqdm object): Iterator with tqdm wrapper if display is True.
+    """
     if display:
         return tqdm(iterator, position=0, 
                     leave=True, desc=desc)
     return iterator
 
+# TODO: Add tests
 class TextCleaner:
     def __init__(self, word_count_min=1, word_length_min=2, bigram_kwargs={}):
         self.remove_punctuation_rule = re.compile(f"[{re.escape(string.punctuation)}]")
@@ -35,21 +50,46 @@ class TextCleaner:
                           'you','your','yours']
         self.STOPWORDS = keep_stopwords = [s for s in STOPWORDS if s not in keep_stopwords]
         
-    def _pos_filter(self, ngram):
-        if ('-pron-' in ngram) or ('t' in ngram):
+    def _pos_filter(self, bigram):
+        """
+        Helper method to filter bigrams based on part of speech (POS).
+        
+        Args:
+            bigram (tuple): bigram to filter. Example: ('word1','word2').
+            
+        Raises:
+            None.
+            
+        Returns:
+            (bool): True/False whether to keep bigram.
+        """
+        # TODO: Is this first step necessary? Is it doing anything?
+        if ('-pron-' in bigram) or ('t' in bigram):
             return False
-        for word in ngram:
+        for word in bigram:
             if (word in self.STOPWORDS) or word.isspace() or (word in string.punctuation):
                 return False
         acceptable_types = ('JJ', 'JJR', 'JJS', 'NN', 'NNS', 'NNP', 'NNPS')
         second_type = ('NN', 'NNS', 'NNP', 'NNPS')
-        tags = nltk.pos_tag(ngram)
+        tags = nltk.pos_tag(bigram)
         if (tags[0][1] in acceptable_types) and (tags[1][1] in second_type):
             return True
         else:
             return False
 
     def _create_bigrams_dict(self, tokens):
+        """
+        Helper method to create map of bigrams to extract.
+        
+        Args:
+            tokens (list of strings): Ordered list of tokens.
+            
+        Raises:
+            None.
+            
+        Returns:
+            bigrams_dict (dict): Dictionary of bigrams to extract. 
+        """
         # Create bigrams
         bigram_finder = nltk.collocations.BigramCollocationFinder.from_words(tokens)
 
@@ -61,9 +101,22 @@ class TextCleaner:
 
         # Filter bigrams based on POS and min PMI
         self.bigrams_dict = {b[0]:f'{b[0][0]} {b[0][1]}' for b in self.bigrams_dict 
-                             if ((b[1]>1) and self._pos_filter(b[0]))}
+                             if ((b[1]>=self.bigrams_pmi_min_value) and self._pos_filter(b[0]))}
         
     def _get_bigrams(self, tokens, fit):
+        """
+        Helper method to extract bigrams from tokens.
+        
+        Args:
+            tokens (list of lists of strings): List of documents split into tokens.
+            fit (bool): Whether to fit new bigrams or use existing mapping to extract bigrams.
+            
+        Raises:
+            None.
+            
+        Returns:
+            bigram_documents (list of lists of strings): Extracted bigrams from tokens.
+        """
         if fit:
             self._create_bigrams_dict(list(itertools.chain(*tokens)))
         bigram_documents = []
@@ -80,6 +133,19 @@ class TextCleaner:
         return bigram_documents
     
     def _preprocess_text(self, documents, fit):
+        """
+        Helper method to preprocess text by removing punctuation, extra spaces, and lower casing.
+        
+        Args:
+            documents (list of strings): Documents to preprocess.
+            fit (bool): Whether to display progress.
+            
+        Raises:
+            None.
+            
+        Returns:
+            tokens (list of lists of strings): Transformed documents into tokens.
+        """
         tokens = []
         for text in _tqdm(documents, display=fit, desc='Preprocessing text'):
             text = self.remove_punctuation_rule.sub(' ', text)
@@ -87,11 +153,24 @@ class TextCleaner:
             tokens.append(text.strip().lower().split())
         return tokens
 
-    def _clean_tokens(self, documents, fit):
+    def _filter_tokens(self, tokens, fit):
+        """
+        Helper method to filter tokens based on stopwords and word_length_min, and fit word_counts.
+        
+        Args:
+            tokens (list of lists of strings): List of documents split into tokens.
+            fit (bool): Whether to fit word_counts.
+            
+        Raises:
+            None.
+        
+        Returns:
+            cleaned_tokens (list of lists of strings): Cleaned tokens.
+        """
         cleaned_tokens = []
-        for tokens in _tqdm(documents, display=fit, desc='Filtering tokens'):
+        for token_list in _tqdm(tokens, display=fit, desc='Filtering tokens'):
             temp_tokens = []
-            for word in tokens:
+            for word in token_list:
                 if (word not in self.STOPWORDS) & (len(word) >= self.word_length_min):
                     temp_tokens.append(word)
                     if fit:
@@ -100,14 +179,40 @@ class TextCleaner:
         return cleaned_tokens
     
     def _filter_word_count(self, cleaned_tokens, fit):
+        """
+        Helper method to filter tokens based on word_count_min.
+        
+        Args:
+            cleaned_tokens (list of lists of strings): List of documents split into cleaned tokens.
+            fit (bool): Whether to display progress.
+            
+        Raises:
+            None.
+        
+        Returns:
+            (list of lists of strings): Filtered tokens.
+        """
         return [[word for word in ct if (self.word_counts.get(word, 0) > self.word_count_min)] 
                 for ct in _tqdm(cleaned_tokens, display=fit, desc='Filtering by word count')]
 
     def _clean_documents(self, documents, fit):
-        documents = self._preprocess_text(documents, fit)
-        cleaned_tokens = self._clean_tokens(documents, fit)
+        """
+        Helper method to clean documents.
+        
+        Args:
+            documents (list of strings): Documents to clean.
+            fit (bool): Whether to fit cleaner and display progress.
+            
+        Raises:
+            None.
+            
+        Returns:
+            cleaned_tokens (list of lists of strings): Cleaned tokens.
+        """
+        tokens = self._preprocess_text(documents, fit)
+        cleaned_tokens = self._filter_tokens(tokens, fit)
         if self.bigrams:
-            bigram_tokens = self._get_bigrams(documents, fit)
+            bigram_tokens = self._get_bigrams(tokens, fit)
             cleaned_tokens = [doc[0]+doc[1] for doc in zip(bigram_tokens, cleaned_tokens)]
         if self.word_count_min <= 1:
             return cleaned_tokens
@@ -115,9 +220,33 @@ class TextCleaner:
             return self._filter_word_count(cleaned_tokens, fit)
         
     def fit_transform(self, documents):
+        """
+        Fits TextCleaner and transforms documents.
+        
+        Args:
+            documents (list of strings): Documents to fit and transform.
+            
+        Raises:
+            None.
+            
+        Returns:
+            cleaned_tokens (list of lists of strings): Cleaned tokens.
+        """
         cleaned_tokens = self._clean_documents(documents, True)
         self.word_counts = dict(self.word_counts)
         return cleaned_tokens
     
     def transform(self, documents):
+        """
+        Transforms documents given the fit TextCleaner.
+        
+        Args:
+            documents (list of strings): Documents to transform.
+            
+        Raises:
+            None.
+            
+        Returns:
+            (list of lists of strings): Cleaned tokens.
+        """
         return self._clean_documents(documents, False)
